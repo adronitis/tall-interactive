f6e97f8b54216b500c7bbb170290d093d761a938
JC <jc@knockloud.com>
2023-09-14 09:24:52 +0200

    Remove mountHasSlot()

--- src/Livewire/Concerns/HasSlot.php
+++ src/Livewire/Concerns/HasSlot.php
@@ -2,16 +2,9 @@
 
 namespace RalphJSmit\Tall\Interactive\Livewire\Concerns;
 
-use Illuminate\Support\HtmlString;
-
 trait HasSlot
 {
     public string $slot = '';
-
-    public function mountHasSlot(HtmlString $slot): void
-    {
-        $this->slot = $slot->toHtml();
-    }
 
     public function submitSlot(): void
     {
